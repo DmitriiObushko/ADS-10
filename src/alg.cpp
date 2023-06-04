db3a990351c2a5de1cdb88428be10ef23ecfac5d
DmitriiObushko <105298559+DmitriiObushko@users.noreply.github.com>
2023-06-04 18:21:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,21 +1,15 @@
 // Copyright 2022 NNTU-CS
-#include <iostream>
-#include <fstream>
-#include <locale>
-#include <cstdlib>
-#include <vector>
-#include "tree.h"
+#include  <iostream>
+#include  <fstream>
+#include  <locale>
+#include  <cstdlib>
+#include  "tree.h"
+
 std::vector<char> getPerm(const Tree& tree, int n) {
-    std::vector<char> result;
-    if (tree.permutations.size() < n ) {
-    if ( tree.permutations.size() < n ) {
-       result.push_back('1');
-       result.clear();
-    } else {
-        result = tree.permutations[n - 1];
-        reverse(result.begin(), result.end());
-        result.pop_back();
-        reverse(result.begin(), result.end());
-    }
-    return result;
+  std::vector<std::vector<char>> perms = tree.getPerms();
+  if (perms.size() >= n) {
+    return perms[n - 1];
+  } else {
+    return {};
+  }
 }
